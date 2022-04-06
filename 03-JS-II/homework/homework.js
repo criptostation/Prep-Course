abb2ab3cb3e61f7927bbaca569728ff8d1d1f9e2
criptostation <enriqurdrgz3@gmail.com>
2022-04-06 03:24:50 -0500

    new duc

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -61,18 +61,31 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case "blue" : 
+    return "This is blue" ;
+    case "red" :
+    return "This is red" ;
+  case "green" :
+    return "This is green" ;
+    case "orange": 
+    return "This is orange" ;
+   }
+   return "Color not found"
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+ return numero === 10 || numero === 5 ;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero < 50 && numero > 20 ;
 }
 
 function esEntero(numero) {
@@ -83,6 +96,8 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return Math.floor(numero) === numero
+
 }
 
 function fizzBuzz(numero) {
@@ -90,6 +105,15 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero % 3 === 0 && numero % 5 === 0 ){
+    return "fizzbuzz" ;
+  }
+  if ( numero % 5 === 0){
+    return "buzz" ; }
+    if( numero % 3 === 0) {
+      return "fizz" ;
+    }
+    return numero
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -99,7 +123,19 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-}
+  if (num1 === 0 || num2 === 0 || num3 === 0 ) {
+    return "Error" ;
+  }
+ else if (num1 < 0 || num2 < 0 || num3 < 0 ) {
+    return "Hay negativos" ; }
+  else if(num1 > num2 && num1 > num3 && num1 >= 0 ) {
+    return "Número 1 es mayor y positivo" ; 
+    }else if (num3 > num1 && num3 > num2 ) {
+      num3 = num3 +1 ;
+      return num3 ;
+    }
+    return false
+} 
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
